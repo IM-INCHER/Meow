44ff877a5f27963f9b0b2bbf76bd88c5771beff8
doraji1247 <zeaphsbgy@naver.com>
2023-10-11 22:46:26 +0900

    BGY
    BGY

--- Meow/Assets/IC/Script/PlayerController.cs
+++ Meow/Assets/IC/Script/PlayerController.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float JumpPower;
+    public float jumpCount;
+    public bool isLongJump = false;
+    public bool isJumping = false;
+    int health = 3;
+
+    Rigidbody2D rb;
+    Animator anim;
 
     void Start()
     {
         
+    }
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        jumpCount = 1;
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -16,5 +31,70 @@ public class PlayerController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         Vector2 movement = new Vector2(horizontalInput * moveSpeed, 0f);
         transform.Translate(movement * Time.deltaTime);
+        
+        if(Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isJumping == false)
+            {
+                isJumping = true;
+                GetComponent<Rigidbody2D>().AddForce(Vector3.up * 450f);
+                isLongJump = true;
+                anim.SetBool("Jumping", true);
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isLongJump = false;
+            anim.SetBool("Jumping", false);
+        }
+    }
+
+    public void Jump()
+    {
+        rb.velocity = Vector2.up * JumpPower;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isLongJump && rb.velocity.y > 0)
+        {
+            rb.gravityScale = 2.0f;
+        }
+        else
+        {
+            rb.gravityScale = 5.0f;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Box")
+        {
+            Debug.Log("¹Ú½º");
+            if (health > 0)
+            {
+                health -= 1;
+                HPManager.hp -= 1;
+
+            }
+            else if (HPManager.hp == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        if (col.gameObject.tag == "ground")
+        {
+            isJumping = false;
+        }
     }
 }
