42ee38a60784c8424d51fa55cd304dab791cd654
INCHUL <53209052+IM-INCHER@users.noreply.github.com>
2023-11-27 15:32:53 +0900

    수정

--- Meow/Assets/IC/Script/Mushroom.cs
+++ Meow/Assets/IC/Script/Mushroom.cs
@@ -13,7 +13,7 @@ public class Mushroom : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && GameManager.instance.catState != Cat_State.Liquid)
         {
             if (GameManager.instance.hp > 0)
             {
